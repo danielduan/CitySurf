f73e04380c2b9e6a8b66dc2da1e14a2dd616e578
Daniel Duan <danielduan88@yahoo.com>
2013-06-02 17:48:56 -0700

    index change

--- index.php
+++ index.php
@@ -328,6 +328,24 @@
   ga('send', 'pageview');
 
 </script>
+
+<!-- Start of StatCounter Code for Default Guide -->
+<script type="text/javascript">
+var sc_project=8996405; 
+var sc_invisible=1; 
+var sc_security="86f655a9"; 
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
+</script>
+<noscript><div class="statcounter"><a title="hit counter"
+href="http://statcounter.com/" target="_blank"><img
+class="statcounter"
+src="http://c.statcounter.com/8996405/0/86f655a9/1/"
+alt="hit counter"></a></div></noscript>
+<!-- End of StatCounter Code for Default Guide -->
 </body>
 
 
